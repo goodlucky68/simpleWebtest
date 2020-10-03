fcbce783f8f8c735980de84929028e0bc65c59e7
goodlucky68 <wesjfc@msn.com>
2020-10-03 15:36:54 +0800

    修改地方 原来：System.out.println("Hello World11, TestNG"); 现在：System.out.println("Hello World11-show in history for the first time, TestNG");

--- src/test/java/simplewebtest/test/TestWebdriverEnv.java
+++ src/test/java/simplewebtest/test/TestWebdriverEnv.java
@@ -10,7 +10,7 @@ public class TestWebdriverEnv {
 	public void checkEnv(){
 		//首先打一个hello world来测试一你的IDE里testng的插件是否已经安装好
 		//check if you success installed testng on your IDE
-		System.out.println("Hello World, TestNG");
+		System.out.println("Hello World11-show in history for the first time, TestNG");
 		
 		//看看你电脑上能不能正确用firefox driver启动firefox
 		//check if firefox driver runs successfully on your computer
